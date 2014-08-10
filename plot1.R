310478f48e096a762c377021890880616cfad121
cg80 <callum.gilhooley@gmail.com>
2014-08-10 18:57:48 +0100

    Plot 1
    committed plot1.R and plot1.png files to directory

---
+++ plot1.R
@@ -0,0 +1,14 @@
+## import the data to R
+data <- read.csv("household_power_consumption.txt", sep = ";", stringsAsFactors=F, na.strings="?")
+
+## subset the desired date range
+pwr <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007",]
+
+## Convert the Date column from char to Date class
+pwr$Date <- as.Date(pwr$Date, format="%d/%m/%Y")
+
+## Create a PNG of the hitogram plot
+png("plot1.png", width=480, height=480)
+hist(pwr$Global_active_power, main="Global Active Power", col="red", 
+     xlab="Global Active Power (kilowatts)", ylab="Frequency")
+dev.off()
